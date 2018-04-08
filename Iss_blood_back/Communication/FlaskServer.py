56f6b8783d5df6892bc478d88d0c1bd2a37dd65d
Andrei Moldovan <moldovan.andrei1998@yahoo.ro>
2018-04-08 23:13:39 +0300

    added logging

--- Iss_blood_back/Communication/FlaskServer.py
+++ Iss_blood_back/Communication/FlaskServer.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import logging
 
 
 class FlaskServer:
@@ -8,17 +9,26 @@ class FlaskServer:
         self.port = config_data["flask_http_port"]
         self.host = '0.0.0.0'
         self.request_data = {}
+        self.logger = logging.getLogger()
 
         self.init_requests()
 
     def run(self):
         self.app.run(self.host, self.port)
 
+    def shutdown_server(self):
+        func = request.environ.get('werkzeug.server.shutdown')
+        if func is None:
+            raise RuntimeError('Not running with the Werkzeug Server')
+        func()
+
+    def stop(self):
+        self.shutdown_server()
+
     def init_requests(self):
         self.app.add_url_rule("/test", "test_request", self.test_request, methods=["GET", "POST"])
 
     def test_request(self):
-        print("inside test")
         self.request_data = request.get_json()
-        print(self.request_data)
+        self.logger.debug("Req data: {}".format(self.request_data))
         return "ok"
--- Iss_blood_back/main.py
+++ Iss_blood_back/main.py
@@ -1,8 +1,12 @@
+import time
+import logging
 import argparse
 import os
 import json
 
 from Communication.FlaskServer import FlaskServer
+
+logger = None
 
 
 def parse_input_file(config_file):
@@ -17,7 +21,7 @@ def parse_input_file(config_file):
             elif "flask" in key:
                 flask_config[key] = j[key]
             else:
-                print("Invalid Json key '{}' in config file.".format(key))
+                logger.warning("Invalid Json key '{}' in config file.".format(key))
 
     return flask_config, db_config
 
@@ -27,8 +31,26 @@ def init_flask_server(flask_config):
     flask.run()
 
 
-def main():
+def init_logger():
+    global logger
+    logger = logging.getLogger()
 
+    log_format_string = '[ %(levelname)8s ] | %(asctime)20s | %(filename)15s | %(lineno)4d | %(funcName)15s() | %(message)s'
+
+    log_file_name = os.path.join(os.getcwd(), 'Logs', 'iss_blood_%s.log' % time.strftime('%Y-%m-%d %H-%M-%S'))
+    logger.setLevel(logging.DEBUG)
+
+    logging.basicConfig(
+        format=log_format_string,
+        handlers=[
+            logging.FileHandler(log_file_name),
+            logging.StreamHandler()
+        ])
+
+    return logger
+
+
+def main():
     parser = argparse.ArgumentParser(description="ISS Blood Server")
     parser.add_argument('-c', metavar='--config_file', type=str,
                         help='Config file for flask server and database connection')
@@ -40,16 +62,17 @@ def main():
 
     config_file = args.c
 
-    # init_loger()
+    logger = init_logger()
+    logger.debug("Logger was initialised.")
 
     if not os.path.exists(config_file):
-        print("Error: Path '{}' does not exist".format(config_file))
+        logger.error("Error: Path '{}' does not exist".format(config_file))
         return
 
     flask_config, db_config = parse_input_file(config_file)
-
-    print(flask_config)
-    print(db_config)
+    logger.debug('Input file {} was parsed.'.format(config_file))
+    logger.debug("Flask_config: {}".format(flask_config))
+    logger.debug("db_config: {}".format(db_config))
 
     init_flask_server(flask_config)
 
