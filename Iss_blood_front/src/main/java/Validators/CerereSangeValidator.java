a97f391d51d8b671ef6591af465a7ca92ccd9694
Andrei Moldovan <moldovan.andrei1998@yahoo.ro>
2018-05-27 14:54:51 +0300

    trash

--- Iss_blood_front/src/main/java/Validators/CerereSangeValidator.java
+++ Iss_blood_front/src/main/java/Validators/CerereSangeValidator.java
@@ -11,22 +11,6 @@ public class CerereSangeValidator extends Validator<CerereSange> {
 
         if(cerere.getNumePacient().equals(""))
 
-        if(info.getAddress().equals("") || info.getEmail().equals("") || info.getNume().equals("") ||
-                info.getPassword().equals("") || info.getPhone().equals("") || info.getUsername().equals(""))
-            erori += "Toate campurile sunt obligatorii\n";
-
-        if(info.getPhone().length() != 10)
-            erori += "Numarul de telefon trebuie a aiba 10 cifre\n";
-
-        if(!checkForDigits(info.getPhone()))
-            erori += "Numarul de telefon nu e valid\n";
-
-        if(info.getCnp().length() != 13)
-            erori += "CNP-ul trebuie sa aiba 13 cifre\n";
-
-        if(!checkForDigits(info.getCnp()))
-            erori += "CNP-ul nu e valid\n";
-
         if (erori.equals(""))
             return new Pair<>(true, "Ok");
         return new Pair<>(false, erori);
