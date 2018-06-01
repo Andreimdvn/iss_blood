e46531bb45af7443a2a0fbdec106b9899c5e6422
Munteanu Ciprian <justashadownfg@gmail.com>
2018-06-01 22:23:04 +0300

    Am scos dummy table

--- Iss_blood_front/src/main/java/Controller/IstoricDonariController.java
+++ Iss_blood_front/src/main/java/Controller/IstoricDonariController.java
@@ -18,20 +18,7 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class IstoricDonariController extends ControlledScreen {
-
-    //TO DO: CE PLM e cu Vboxul si hardocarea asta
-
-    @FXML
-    private VBox tabelDonari;
-
-    private final int MAX_SIZE = 7;
     Logger logger = LogManager.getLogger(IstoricDonariController.class.getName());
-    private boolean isFull(){
-        return getSize() == MAX_SIZE;
-    }
-    private int getSize(){
-        return tabelDonari.getChildren().size() - 2;
-    }
 
     @FXML
     private TableView viewIstoric;
@@ -55,48 +42,9 @@ public class IstoricDonariController extends ControlledScreen {
     private Predicate<DonareInfo> filterCentru = info -> true;
     private Predicate<DonareInfo> filterStatus = info -> true;
 
-
-
     private Collection<DonareInfo> istoric;
     private ObservableList<DonareInfo> model = FXCollections.observableArrayList();
 
-
-    int contor = 0;
-    @FXML
-    private void dummyTest()
-    {
-        if(!isFull()) {
-            contor ++;
-            createHBoxIstoric(contor, "01/01/1990",
-                    "Nicu camin 16", "Ready to view");
-        }
-    }
-    private void createHBoxIstoric(int numarDonare,String dataDonare,String numeCentru,String status){
-
-        logger.debug("Buton dummy a fost apasat");
-        HBox hBox = new HBox();
-        String numeStyle = "hboxIstoric";
-        hBox.getStyleClass().add(numeStyle);
-        Label id = new Label(String.valueOf(numarDonare));
-        Label data = new Label(dataDonare);
-        Label nume = new Label(numeCentru);
-        Label st = new Label(status);
-
-
-        String numeLabelStyle = "labelIstoric";
-        id.getStyleClass().add(numeLabelStyle);
-        data.getStyleClass().add(numeLabelStyle);
-        nume.getStyleClass().add(numeLabelStyle);
-        st.getStyleClass().add(numeLabelStyle);
-
-        id.setPrefWidth(114);
-        data.setPrefWidth(114);
-        nume.setPrefWidth(225);
-        st.setPrefWidth(84);
-
-        hBox.getChildren().addAll(id,data,nume,st);
-        tabelDonari.getChildren().add(getSize() + 1,hBox);
-    }
 
     @FXML
     private void initialize(){
@@ -114,7 +62,6 @@ public class IstoricDonariController extends ControlledScreen {
         lst.add(Status.PREGATIRE);
         lst.add(Status.PRELEVARE);
         statusComboBox.setItems(lst);
-
     }
 
     @FXML
